36036331ae03d3fe253e812ac5b783fd9f85cf67
Alex Schroeder <alex@gnu.org>
2018-03-02 00:21:08 +0100

    Added license to source file

--- eval-sexp-in-comments.el
+++ eval-sexp-in-comments.el
@@ -2,6 +2,19 @@
 
 ;; Copyright (C) 2001  Alex Schroeder
 
+;; This program is free software: you can redistribute it and/or modify it under
+;; the terms of the GNU General Public License as published by the Free Software
+;; Foundation, either version 3 of the License, or (at your option) any later
+;; version.
+;;
+;; This program is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+;; FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+;; details.
+;;
+;; You should have received a copy of the GNU General Public License along with
+;; GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+
 ;; Emacs Lisp Archive Entry
 ;; Filename: eval-sexp-in-comments.el
 ;; Version: 1.0.0
